be24c632615586ec06d52eda6ce37a29ff3a8117
Creed Petitt <creedpetitt@gmail.com>
2026-02-20 16:25:41 -0600

    define Bus struct and read/write functions

---
+++ src/bus.rs
@@ -0,0 +1,20 @@
+pub struct Bus {
+    pub memory: [u8; 65536],
+}
+
+impl Bus {
+    pub fn new() -> Self {
+        Bus{
+            memory: [0; 65536],
+        }
+    }
+    pub fn read(&self, address: u16) -> u8 {
+        // Cast address to usize for array indexing
+        self.memory[address as usize]
+    }
+
+
+    pub fn write(&mut self, address: u16, data: u8) {
+        self.memory[address as usize] = data;
+    }
+}
