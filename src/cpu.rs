6d4817ddea342a7e2965c7d26c13c526198bbfd6
Creed Petitt <creedpetitt@gmail.com>
2026-02-20 17:46:59 -0600

    define status flag bits and implement fetch and step functions

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,14 @@
 use crate::bus::Bus;
+
+pub const FLAG_CARRY: u8     = 0b0000_0001;
+pub const FLAG_ZERO: u8      = 0b0000_0010;
+pub const FLAG_INTERRUPT: u8 = 0b0000_0100;
+pub const FLAG_DECIMAL: u8   = 0b0000_1000;
+pub const FLAG_BREAK: u8     = 0b0001_0000;
+pub const FLAG_UNUSED: u8    = 0b0010_0000;
+pub const FLAG_OVERFLOW: u8  = 0b0100_0000;
+pub const FLAG_NEGATIVE: u8  = 0b1000_0000;
+
 pub struct CPU {
     pub register_a: u8,        // 8-bit Accumulator
     pub register_x: u8,        // 8-bit X Register
@@ -9,7 +19,7 @@ pub struct CPU {
     pub bus: Bus,              // The physical connection to Memory
 }
 
-impl CPU{
+impl CPU {
     pub fn new(bus: Bus) -> Self {
         CPU {
             register_a: 0,
@@ -20,5 +30,17 @@ impl CPU{
             program_counter: 0,
             bus,
         }
+    }
+
+    fn fetch(&mut self) -> u8 {
+        let address = self.program_counter;
+        let data = self.bus.read(address);
+        self.program_counter += 1;
+        data
+    }
+
+    pub fn step(&mut self) {
+        let opcode = self.fetch();
+        crate::opcodes::execute(self, opcode);
     }
 }
