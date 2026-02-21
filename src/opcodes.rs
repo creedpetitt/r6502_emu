a9082184fb644b48d1b7940b1671d5e3ce3721dd
Creed Petitt <creedpetitt@gmail.com>
2026-02-21 00:13:59 -0600

    add zero-page and absolute LDX and LDY instructions

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -3,31 +3,85 @@ use crate::addressing::{AddressingMode, get_operand_address};
 
 pub fn execute(cpu: &mut CPU, opcode: u8) {
     match opcode {
-        // Load Accumulator
-        0xA9 => { // LDA Immediate
+        // LDA (Load A)
+        0xA9 => { // Immediate
             let addr = get_operand_address(cpu, &AddressingMode::Immediate);
             cpu.register_a = cpu.bus.read(addr);
             update_zero_and_negative_flags(cpu, cpu.register_a);
         }
+        0xA5 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.register_a = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_a);
+        }
+        0xAD => { // Absolute
+            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
+            cpu.register_a = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_a);
+        }
 
-        // Load X Register
-        0xA2 => { // LDX Immediate
+        // LDX (Load X)
+        0xA2 => { // Immediate
             let addr = get_operand_address(cpu, &AddressingMode::Immediate);
             cpu.register_x = cpu.bus.read(addr);
             update_zero_and_negative_flags(cpu, cpu.register_x);
         }
+        0xA6 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.register_x = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_x);
+        }
+        0xAE => { // Absolute
+            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
+            cpu.register_x = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_x);
+        }
 
-        // Load Y Register
-        0xA0 => { // LDY Immediate
+        // LDY (Load Y)
+        0xA0 => { // Immediate
             let addr = get_operand_address(cpu, &AddressingMode::Immediate);
             cpu.register_y = cpu.bus.read(addr);
             update_zero_and_negative_flags(cpu, cpu.register_y);
         }
+        0xA4 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.register_y = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_y);
+        }
+        0xAC => { // Absolute
+            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
+            cpu.register_y = cpu.bus.read(addr);
+            update_zero_and_negative_flags(cpu, cpu.register_y);
+        }
 
-        // Store Accumulator
-        0x8D => { // STA Absolute
+        // STA (Store A)
+        0x85 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.bus.write(addr, cpu.register_a);
+        }
+        0x8D => { // Absolute
             let addr = get_operand_address(cpu, &AddressingMode::Absolute);
             cpu.bus.write(addr, cpu.register_a);
+        }
+
+        // STX (Store X)
+        0x86 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.bus.write(addr, cpu.register_x);
+        }
+        0x8E => { // Absolute
+            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
+            cpu.bus.write(addr, cpu.register_x);
+        }
+
+        // STY (Store Y)
+        0x84 => { // Zero Page
+            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
+            cpu.bus.write(addr, cpu.register_y);
+        }
+        0x8C => { // Absolute
+            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
+            cpu.bus.write(addr, cpu.register_y);
         }
 
         _ => { }
