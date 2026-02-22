24566177043cfcfe611ab7ff19a162e6f56085f9
Creed Petitt <creedpetitt@gmail.com>
2026-02-21 20:07:26 -0600

    extract load and store logic into helper functions to reduce repeated code

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -3,133 +3,81 @@ use crate::addressing::{AddressingMode, get_operand_address};
 
 pub fn execute(cpu: &mut CPU, opcode: u8) {
     match opcode {
-        // LDA (Load A)
-        0xA9 => { // Immediate
-            let addr = get_operand_address(cpu, &AddressingMode::Immediate);
-            cpu.register_a = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_a);
-        }
-        0xA5 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.register_a = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_a);
-        }
-        0xAD => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.register_a = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_a);
-        }
+        // LDA
+        0xA9 => cpu.register_a = load(cpu, &AddressingMode::Immediate),
+        0xA5 => cpu.register_a = load(cpu, &AddressingMode::ZeroPage),
+        0xAD => cpu.register_a = load(cpu, &AddressingMode::Absolute),
 
-        // LDX (Load X)
-        0xA2 => { // Immediate
-            let addr = get_operand_address(cpu, &AddressingMode::Immediate);
-            cpu.register_x = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_x);
-        }
-        0xA6 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.register_x = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_x);
-        }
-        0xAE => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.register_x = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_x);
-        }
+        // LDX
+        0xA2 => cpu.register_x = load(cpu, &AddressingMode::Immediate),
+        0xA6 => cpu.register_x = load(cpu, &AddressingMode::ZeroPage),
+        0xAE => cpu.register_x = load(cpu, &AddressingMode::Absolute),
 
-        // LDY (Load Y)
-        0xA0 => { // Immediate
-            let addr = get_operand_address(cpu, &AddressingMode::Immediate);
-            cpu.register_y = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_y);
-        }
-        0xA4 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.register_y = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_y);
-        }
-        0xAC => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.register_y = cpu.bus.read(addr);
-            update_zero_and_negative_flags(cpu, cpu.register_y);
-        }
+        // LDY
+        0xA0 => cpu.register_y = load(cpu, &AddressingMode::Immediate),
+        0xA4 => cpu.register_y = load(cpu, &AddressingMode::ZeroPage),
+        0xAC => cpu.register_y = load(cpu, &AddressingMode::Absolute),
 
-        // STA (Store A)
-        0x85 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.bus.write(addr, cpu.register_a);
-        }
-        0x8D => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.bus.write(addr, cpu.register_a);
-        }
+        // STA
+        0x85 => store(cpu, &AddressingMode::ZeroPage, cpu.register_a),
+        0x8D => store(cpu, &AddressingMode::Absolute, cpu.register_a),
 
-        // STX (Store X)
-        0x86 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.bus.write(addr, cpu.register_x);
-        }
-        0x8E => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.bus.write(addr, cpu.register_x);
-        }
+        // STX
+        0x86 => store(cpu, &AddressingMode::ZeroPage, cpu.register_x),
+        0x8E => store(cpu, &AddressingMode::Absolute, cpu.register_x),
 
-        // STY (Store Y)
-        0x84 => { // Zero Page
-            let addr = get_operand_address(cpu, &AddressingMode::ZeroPage);
-            cpu.bus.write(addr, cpu.register_y);
-        }
-        0x8C => { // Absolute
-            let addr = get_operand_address(cpu, &AddressingMode::Absolute);
-            cpu.bus.write(addr, cpu.register_y);
-        }
+        // STY
+        0x84 => store(cpu, &AddressingMode::ZeroPage, cpu.register_y),
+        0x8C => store(cpu, &AddressingMode::Absolute, cpu.register_y),
 
-        // TAX (Transfer A to X)
-        0xAA => {
+        0xAA => { // TAX
             cpu.register_x = cpu.register_a;
             update_zero_and_negative_flags(cpu, cpu.register_x);
         }
-        // TAY (Transfer A to Y)
-        0xA8 => {
+        0xA8 => { // TAY
             cpu.register_y = cpu.register_a;
             update_zero_and_negative_flags(cpu, cpu.register_y);
         }
-
-        // TXA (Transfer X to A)
-        0x8A => {
+        0x8A => { // TXA
             cpu.register_a = cpu.register_x;
             update_zero_and_negative_flags(cpu, cpu.register_a);
         }
-        // TYA (Transfer Y to A)
-        0x98 => {
+        0x98 => { // TYA
             cpu.register_a = cpu.register_y;
             update_zero_and_negative_flags(cpu, cpu.register_a);
         }
 
-        // INX
-        0xE8 => {
+        0xE8 => { // INX
             cpu.register_x = cpu.register_x.wrapping_add(1);
             update_zero_and_negative_flags(cpu, cpu.register_x);
         }
-        // INY
-        0xC8 => {
+        0xC8 => { // INY
             cpu.register_y = cpu.register_y.wrapping_add(1);
             update_zero_and_negative_flags(cpu, cpu.register_y);
         }
-
-        // DEX
-        0xCA => {
+        0xCA => { // DEX
             cpu.register_x = cpu.register_x.wrapping_sub(1);
             update_zero_and_negative_flags(cpu, cpu.register_x);
         }
-        // DEY
-        0x88 => {
+        0x88 => { // DEY
             cpu.register_y = cpu.register_y.wrapping_sub(1);
             update_zero_and_negative_flags(cpu, cpu.register_y);
         }
 
         _ => { }
     }
+}
+
+fn load(cpu: &mut CPU, mode: &AddressingMode) -> u8 {
+    let addr = get_operand_address(cpu, mode);
+    let value = cpu.bus.read(addr);
+    update_zero_and_negative_flags(cpu, value);
+    value
+}
+
+fn store(cpu: &mut CPU, mode: &AddressingMode, value: u8) {
+    let addr = get_operand_address(cpu, mode);
+    cpu.bus.write(addr, value);
 }
 
 fn update_zero_and_negative_flags(cpu: &mut CPU, result: u8) {
