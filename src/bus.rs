344c358a8260007a8d2b101096eb45ffab2bf6fd
Creed Petitt <creedpetitt@gmail.com>
2026-02-21 20:15:12 -0600

    fix spacing

--- src/bus.rs
+++ src/bus.rs
@@ -4,7 +4,7 @@ pub struct Bus {
 
 impl Bus {
     pub fn new() -> Self {
-        Bus{
+        Bus {
             memory: [0; 65536],
         }
     }
@@ -12,7 +12,6 @@ impl Bus {
         // Cast address to usize for array indexing
         self.memory[address as usize]
     }
-
 
     pub fn write(&mut self, address: u16, data: u8) {
         self.memory[address as usize] = data;
